954a1d9cb2f4897a0122ddb753fbafab51fa64a3
William Bae <wbae912@gmail.com>
2019-10-24 22:14:31 -0700

    Database ready for Heroku deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/tv-tracker',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/tv-tracker',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/tv-tracker-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 };
--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -12,7 +12,7 @@ describe('Auth Endpoints', () => {
   before('Connect to DB before ALL tests', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set('db', db);
   });
